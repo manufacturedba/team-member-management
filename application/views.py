6b88377b476303ab92c3249a754b5b5b120323e4
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-13 13:32:54 -0700

    Stamp member query into server-side render

--- application/views.py
+++ application/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.core import serializers
+
+from .models import Member
 
 def index(request):
+    all_members = Member.objects.all()
     template = loader.get_template("index.html")
-    context = {}
+    context = {"store": serializers.serialize("json", all_members)}
     return HttpResponse(template.render(context, request))
--- frontend/app/routes/list.ts
+++ frontend/app/routes/list.ts
@@ -1,3 +1,24 @@
 import Route from '@ember/routing/route';
 
-export default class ListRoute extends Route {}
+function readStore() : Array | null {
+    const metaElement = document.querySelector('meta[name="store"]') as HTMLMetaElement
+    
+    if (!metaElement) {
+        return null
+    }
+    
+    try {
+        return JSON.parse(metaElement.content)
+    } catch (e) {
+        console.error('Unable to parse found store', e);
+    }
+    
+    return null
+}
+export default class ListRoute extends Route {
+    model() {
+        const rawModels = readStore();
+        
+        return rawModels.map((rawModel: any) => rawModel.fields);
+    }
+}
