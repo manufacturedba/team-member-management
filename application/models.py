f05e1c0a7ad69362fd1e230ca18ca46375cf819e
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-13 12:49:18 -0700

    Add seed for member

--- application/models.py
+++ application/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+"""
+Limits here should not be reused for client enforcement
+
+"""
+class Member(models.Model):
+    first_name = models.CharField(max_length=3000)
+    last_name = models.CharField(max_length=3000)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=3000)
+    profile_image = models.CharField(max_length=3000) # TODO: Switch to ImageField
