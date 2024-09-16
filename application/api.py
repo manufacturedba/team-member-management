144ce5b1a0c70c92dc6495e70d7222fd8463b6ea
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-16 10:01:46 -0700

    Create routes for writing of members

--- application/api.py
+++ application/api.py
@@ -1,15 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.core.serializers import serialize
+
 import json
 
 from .models import Member
 
+def getTeamMembers():
+    try:
+        return HttpResponse(serialize("json", Member.objects.all()), status=200)
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
+    
 def addTeamMember(request):
     try :
+        # Blanket assume serialization and saving are client errors
         deserialized = json.loads(request.body)
         member = Member(**deserialized)
         member.save()
-        return HttpResponse("Created", status=200)
+        try:
+            return HttpResponse(serialize("json", [member]), status=200)
+        except Exception as e:
+            print(e)
+            return HttpResponse(status=500)
     except Exception as e:
         print(e)
         return HttpResponse("Bad request", status=400)
+    
+    
+def handler(request):
+    if request.method == "GET":
+        return getTeamMembers()
+    elif request.method == "PUT":
+        return addTeamMember(request)
+    else:
+        return HttpResponse(status=501)
--- application/urls.py
+++ application/urls.py
@@ -1,10 +1,10 @@
 from django.urls import re_path, path, include
 
 from .views import AddView, ListView
-from .api import addTeamMember
+from .api import handler
 
 urlpatterns = [
     path("", ListView.as_view()),
-    path("api/members", addTeamMember),
+    path("api/members", handler),
     re_path(r"^(?P<slug>\w+)/$", AddView.as_view()),
 ]
