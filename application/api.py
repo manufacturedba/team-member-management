76ed2cabdd8b665d790453e4e76758eaac7e2241
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-16 12:56:18 -0700

    Separate instance and collection handler for member

--- application/api.py
+++ application/api.py
@@ -12,6 +12,13 @@ def get_team_members():
         print(e)
         return HttpResponse(status=500)
     
+def get_team_member_by_id(member_pk):
+    try:
+        return HttpResponse(serialize("json", [Member.objects.get(pk=member_pk)]), status=200)
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
+    
 def add_team_member(request):
     try :
         # Blanket assume serialization and saving are client errors
@@ -28,10 +35,16 @@ def add_team_member(request):
         return HttpResponse("Bad request", status=400)
     
     
-def handler(request):
+def collection_handler(request):
     if request.method == "GET":
         return get_team_members()
     elif request.method == "PUT":
         return add_team_member(request)
+    else:
+        return HttpResponse("Not implemented", status=501)
+
+def instance_handler(request, member_pk):
+    if request.method == "GET":
+        return get_team_member_by_id(member_pk)
     else:
         return HttpResponse("Not implemented", status=501)
--- application/urls.py
+++ application/urls.py
@@ -1,11 +1,12 @@
-from django.urls import re_path, path, include
+from django.urls import path
 
 from .views import EmptyContextView, ListView, EditView
-from .api import handler
+from .api import collection_handler, instance_handler
 
 urlpatterns = [
     path("", ListView.as_view()),
     path("add", EmptyContextView.as_view()),
     path("edit/<int:member_pk>", EditView.as_view()),
-    path("api/members", handler),
+    path("api/members/<int:member_pk>/", instance_handler),
+    path("api/members/", collection_handler),
 ]
