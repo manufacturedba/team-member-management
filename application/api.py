df8c6837d6c93bd6da8ac97f6583e2daa892c1ac
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-16 16:26:48 -0700

    Add update/delete operations

--- application/api.py
+++ application/api.py
@@ -19,6 +19,26 @@ def get_team_member_by_id(member_pk):
         print(e)
         return HttpResponse(status=500)
     
+
+def update_team_member_by_id(member_pk, request):
+    try:
+        deserialized = json.loads(request.body)
+        member_to_update = Member.objects.filter(pk=member_pk)
+        member_to_update.update(**deserialized)
+        
+        return HttpResponse(serialize("json", member_to_update), status=200)
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
+    
+def delete_team_member_by_id(member_pk):
+    try: 
+        Member.objects.get(pk=member_pk).delete()
+        return HttpResponse(status=200)
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
+    
 def add_team_member(request):
     try :
         # Blanket assume serialization and saving are client errors
@@ -38,7 +58,7 @@ def add_team_member(request):
 def collection_handler(request):
     if request.method == "GET":
         return get_team_members()
-    elif request.method == "PUT":
+    elif request.method == "POST":
         return add_team_member(request)
     else:
         return HttpResponse("Not implemented", status=501)
@@ -46,5 +66,9 @@ def collection_handler(request):
 def instance_handler(request, member_pk):
     if request.method == "GET":
         return get_team_member_by_id(member_pk)
+    if request.method == "PUT":
+        return update_team_member_by_id(member_pk, request)
+    if request.method == "DELETE":
+        return delete_team_member_by_id(member_pk)
     else:
         return HttpResponse("Not implemented", status=501)
--- application/tests.py
+++ application/tests.py
@@ -1,9 +1,10 @@
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory, TestCase, Client
 
-from .views import ListView, EmptyContextView
-from .api import get_team_members, add_team_member
-from .models import Member
+from .views import ListView, EmptyContextView, EditView
+from .api import get_team_members, add_team_member, delete_team_member_by_id, update_team_member_by_id
 
+from .models import Member  
+           
 class TestViews(TestCase):
     def test_template_for_list_view(self):
         request = RequestFactory().get("/")
@@ -28,7 +29,7 @@ class TestViews(TestCase):
 
         template_name = view.get_template_names()
         self.assertIn("index.html", template_name)
-        
+      
 class TestAPI(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -52,3 +53,33 @@ class TestAPI(TestCase):
         
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"J", response.content)
+        
+    def test_delete_team_member_by_id(self):
+        response = delete_team_member_by_id(1)
+        
+        self.assertEqual(response.status_code, 200)
+        
+    def test_update_team_member_by_id(self):
+        request = RequestFactory().put("/", 
+                                       b'{"first_name": "UpdatedJD"}')
+        
+        response = update_team_member_by_id(1, request)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"UpdatedJD", response.content)
+        
+"""
+    These tests guarantee kwargs are facilitated to views from url
+"""
+class TestRoutes(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.member = Member.objects.create(first_name="J", last_name="D", pk=1)
+        
+    def test_store_is_loaded_into_edit_view(self):
+        client = Client()
+        response = client.get("/edit/1")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"J", response.content)
+        self.assertIn(b"D", response.content)   
